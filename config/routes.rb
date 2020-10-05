7b641c3db92fb36e09c849a9027425f8cd03ddd1
akitcreate <joann.doggo@gmail.com>
2020-10-05 12:13:26 +0900

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'index_page/show'
-  get 'chart_sample/index'
   resources :performances
   resources :budgets
   resources :contents
-
-  root to: "index_page#show"
+  resources :index_page
+  root 'index_page#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
