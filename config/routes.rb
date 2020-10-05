ada13e7766652a678c9bfe731ece74d98c0f289c
akitcreate <joann.doggo@gmail.com>
2020-10-05 12:14:49 +0900

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'index_pages#show'
   resources :performances
   resources :budgets
   resources :contents
   resources :index_page
-  root 'index_page#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
