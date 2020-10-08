f680f9703091fbb0f096102ae43a306ce585ea9c
akitcreate <joann.doggo@gmail.com>
2020-10-09 00:04:07 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_140852) do
+ActiveRecord::Schema.define(version: 2020_10_08_144200) do
 
   create_table "budgets", force: :cascade do |t|
     t.integer "b_year"
@@ -47,6 +47,9 @@ ActiveRecord::Schema.define(version: 2020_10_08_140852) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
