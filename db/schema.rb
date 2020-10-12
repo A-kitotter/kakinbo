4e2ed4c2e2ce494aed2a72891c4e9d76d2e55ae4
akitcreate <joann.doggo@gmail.com>
2020-10-13 00:02:54 +0900

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_144200) do
+ActiveRecord::Schema.define(version: 2020_10_12_145034) do
 
   create_table "budgets", force: :cascade do |t|
     t.integer "b_year"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2020_10_08_144200) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
